a2fa7ecc6ef704f644dc7a98036db0999fcee566
jorgemeyrelles <jotaengpuc@gmail.com>
2021-09-06 16:57:51 -0300

    Updates

---
+++ precache-manifest.730d0fea8c79f3c4897890c8ddbcf5aa.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb745dbfbf234d6dcbc8edbc1d074476",
+    "url": "/mandd-app/index.html"
+  },
+  {
+    "revision": "4c73ee65c421ddb6c7e0",
+    "url": "/mandd-app/static/css/2.38a25f08.chunk.css"
+  },
+  {
+    "revision": "51f683ebf0d838743038",
+    "url": "/mandd-app/static/css/main.5fc3dec4.chunk.css"
+  },
+  {
+    "revision": "4c73ee65c421ddb6c7e0",
+    "url": "/mandd-app/static/js/2.580508cc.chunk.js"
+  },
+  {
+    "revision": "57fb86a905f06aed4b5091745659b335",
+    "url": "/mandd-app/static/js/2.580508cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51f683ebf0d838743038",
+    "url": "/mandd-app/static/js/main.3af8e10f.chunk.js"
+  },
+  {
+    "revision": "cb17172ae9f7b5fc13dd",
+    "url": "/mandd-app/static/js/runtime-main.6d2b72ac.js"
+  },
+  {
+    "revision": "f87c47c37c0c0530fe7307af260caa25",
+    "url": "/mandd-app/static/media/drinkIcon.f87c47c3.svg"
+  },
+  {
+    "revision": "157945fffa619b919f7bb65fe11b7b43",
+    "url": "/mandd-app/static/media/exploreIcon.157945ff.svg"
+  },
+  {
+    "revision": "c53a4a2dcd8f7ed5fc09e7ef337ff60a",
+    "url": "/mandd-app/static/media/mandd_3_sf.c53a4a2d.png"
+  },
+  {
+    "revision": "b27ba19ea033355b73926443af1756e9",
+    "url": "/mandd-app/static/media/mealIcon.b27ba19e.svg"
+  },
+  {
+    "revision": "74e487e504d5cf16d7832bef2fbacfea",
+    "url": "/mandd-app/static/media/profileIcon.74e487e5.svg"
+  },
+  {
+    "revision": "db81213596cb3beb56440bb39aa70c8c",
+    "url": "/mandd-app/static/media/searchIcon.db812135.svg"
+  },
+  {
+    "revision": "06215ed4cae23455c02abc804445ba74",
+    "url": "/mandd-app/static/media/shareIcon.06215ed4.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/mandd-app/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/mandd-app/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/mandd-app/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/mandd-app/static/media/slick.f97e3bbf.svg"
+  }
+]);
