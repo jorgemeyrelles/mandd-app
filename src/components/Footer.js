1ca2fa4d0eaa444c0252628ccbd0fd253e3b9567
jorgemeyrelles <jotaengpuc@gmail.com>
2021-09-07 09:10:26 -0300

    fix: changing a for div on footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Navbar } from 'react-bootstrap';
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 import mealIcon from '../images/mealIcon.svg';
 import exploreIcon from '../images/exploreIcon.svg';
 import drinkIcon from '../images/drinkIcon.svg';
 
 function Footer() {
+  const history = useHistory();
   return (
     <Navbar
       style={ {
@@ -18,15 +20,30 @@ function Footer() {
       } }
       data-testid="footer"
     >
-      <a href="/mandd-app/comidas">
+      <div
+        role="button"
+        onClick={ () => history.push('/mandd-app/comidas') }
+        onKeyPress={ () => history.push('/mandd-app/comidas') }
+        tabIndex="0"
+      >
         <img data-testid="food-bottom-btn" src={ mealIcon } alt="food icon" />
-      </a>
-      <a href="/mandd-app/explorar">
+      </div>
+      <div
+        role="button"
+        onClick={ () => history.push('/mandd-app/explorar') }
+        onKeyPress={ () => history.push('/mandd-app/explorar') }
+        tabIndex="0"
+      >
         <img data-testid="explore-bottom-btn" src={ exploreIcon } alt="explore icon" />
-      </a>
-      <a href="/mandd-app/bebidas">
+      </div>
+      <div
+        role="button"
+        onClick={ () => history.push('/mandd-app/bebidas') }
+        onKeyPress={ () => history.push('/mandd-app/bebidas') }
+        tabIndex="0"
+      >
         <img data-testid="drinks-bottom-btn" src={ drinkIcon } alt="drink icon" />
-      </a>
+      </div>
     </Navbar>
   );
 }
